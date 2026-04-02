613f12fbde59f92015910e4e8730de4b31ddc57a
Vishal Yadav <ydvishal89@gmail.com>
2026-04-02 14:32:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let select  = document.getElementById("colorSelect");
+let button = document.querSelector('input[type="button"');
+
+button.addEventListner("click" , function(){
+	const choosen = select.selectedIndex;
+	if(choosen != null){
+		select.remove(choosen);
+	}
+})
